3b32b460e27d6fade559251d0add88d3449e20e1
Liam Murray <murrayla@student.unimelb.edu.au>
2023-10-04 14:27:31 +1100

    node_isolation-v1-2-2023OCT04
    Introducing simplex mapping

--- node_isolation.py
+++ node_isolation.py
@@ -1,5 +1,66 @@
 import numpy as np
 
+GP = np.array(
+    [
+        [1/3, 1/3], 
+        [0.6, 0.2],
+        [0.2, 0.6],
+        [0.2, 0.2]
+    ]
+)
+WE = np.array([-27/48, 25/48, 25/48, 25/48])
+ORDER = len(WE)
+
+#   η
+#   1  2                   
+#   |  |`\                 
+#   |  |  `\               
+#   |  5    `4             
+#   |  |      `\            
+#   |  |        `\          
+#   0  0-----3-----1
+#      0-----------1 ξ
+
+
+# [Note: ζ = 1 - ξ - η]
+# [
+#     φ0 = ξ * (2 * ξ - 1), 
+#     φ1 = η * ( 2 * η - 1),
+#     φ2 = ζ * (2 * ζ - 1), 
+#     φ3 = 4 * ξ * η
+#     φ4 = 4 * η * ζ, 
+#     φ5 = 4 * ζ * ξ
+# ] @ Gauss
+
+PHI = np.zeros((ORDER, 6))
+
+# [
+#     δφ1/δξ δφ2/δξ ... δφ6/δξ
+#     δφ1/δη δφ2/δη ... δφ6/δη
+# ] @ Gauss
+
+DEL_PHI = np.zeros((ORDER, 2, 6))
+
+for x in range(0, ORDER, 1):
+    # Shape Functions
+    PHI[x, :] = np.array(
+        [
+            GP[x, 0] * (2 * GP[x, 0] - 1),        
+            GP[x, 1] * (2 * GP[x, 1] - 1),
+            (1 - GP[x, 0] - GP[x, 1]) * (2 * (1 - GP[x, 0] - GP[x, 1]) - 1),
+            4 * GP[x, 0] * GP[x, 1],
+            4 * GP[x, 1] * (1 - GP[x, 0] - GP[x, 1]),
+            4 * (1 - GP[x, 0] - GP[x, 1]) * GP[x, 0]
+        ]
+    )
+    # Derivatives of shape functions
+    DEL_PHI[x, :, :] = np.array(
+        [
+            [4*GP[x, 0] - 1, 0, 4*GP[x, 1] + 4*GP[x, 0] - 3, 4*GP[x, 1], -4*GP[x, 1], 4 - 8*GP[x, 0] - 4*GP[x, 1]], 
+            [0, 4*GP[x, 1] - 1, 4*GP[x, 1] + 4*GP[x, 0] - 3, 4*GP[x, 0], 4 - 8*GP[x, 1] - 4*GP[x, 0], -4*GP[x, 0]]
+        ]
+    )
+
 nodes = open("runtime_files/nashAnnulus2D_cvt2dino.nodes", 'r')
 elems = open("runtime_files/nashAnnulus2D_cvt2dino.ele", 'r')
 n_list = list()
@@ -27,4 +88,4 @@ for i, (x, y, z) in enumerate(np_n[:, 1:]):
 n_sur = set(n_sur)
 mask = np.isin(np_e[:, 0], list(n_sur))
 filt_np_e = np_e[mask]
-print(filt_np_e)
+
